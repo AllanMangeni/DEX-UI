2dcf3e833b4f260da95a1cb78e8ba0d09bb6f7b0
Allan mang'eni <allanwanyonyi@protonmail.com>
2021-11-04 07:16:04 +0000

    integrating Ride to the dapp

---
+++ dex-ui/Ride files/Test Files/test.js
@@ -0,0 +1,10 @@
+const wvs = 1e8 
+describe('some suite', () => {
+    // before(async() => {
+    //     await setupAccounts({foo: 1 * wvs, bar: 2 * wvs})
+    // })
+      
+    it('logs something', async () => {
+        console.log('foo')
+    })
+})
